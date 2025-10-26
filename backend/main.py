b2c67ab36dafd2da0d8c7776f2d33395132370d1
AMANDDHUMAL <129135638+AMN-D@users.noreply.github.com>
2025-10-26 18:00:53 +0530

    Restrict CORS origins to specific localhost URLs

--- backend/main.py
+++ backend/main.py
@@ -12,9 +12,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "*"
-        # "http://localhost:3000",
-        # "http://localhost:5173",
+        "http://localhost:3000",
+        "http://localhost:5173",
         ],  
     allow_credentials=True,
     allow_methods=["*"],
